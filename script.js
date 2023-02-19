5fdaa819c015eb87984413b8465ef6e64003c416
Jones-parker <125539386+Jones-parker@users.noreply.github.com>
2023-02-19 14:13:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<head>
+   This is about my new gaming website called Jreko-gaming.github.io.
+  </head>
+  
+  
+  <body>
+   
+ about my webpage is that my webpage is used for downloading games and is used for checking out new game reviews for the first game review i am doing is about the best current games in the world sure we all know the games i am going to review on but this is my first webpage so that is why i want to review on the games for the first time so please be supportive.
+    </body>
